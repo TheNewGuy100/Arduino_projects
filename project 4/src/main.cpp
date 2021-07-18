b695da603d1e2e33cc37d833170bbb71a57b6c31
Pedro Bohn Costa <pedromenezescosta271@gmail.com>
2021-07-18 04:45:34 -0300

    ending project 4 - using LDR to light a LED

--- project 4/src/main.cpp
+++ project 4/src/main.cpp
@@ -2,12 +2,14 @@
 
 // VARIABLES
   int ldr = A0; //Atribui A0 a variável ldr
+  int led1 = 10;
   int valorldr = 0; //Declara a variável valorldr como inteiro
 
 void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ldr, INPUT);  //Define ldr (pino analógico A0) como saída
+  pinMode(led1, OUTPUT);
 
   Serial.begin(9600); //Inicialização da comunicação serial, com taxa de
 
@@ -22,6 +24,13 @@ void loop() {
 
   Serial.print("Valor lido pelo LDR = "); //Imprime na serial a mensagem
   Serial.println(valorldr); //Imprime na serial os dados de valorldr
+
+  if (valorldr > 500) {
+    digitalWrite(led1, LOW);
+  } else {
+    digitalWrite(led1, HIGH);
+  }
+  
   // INDICADOR DE LOOP DA PLACA
   digitalWrite(LED_BUILTIN, LOW);
   delay(150);
